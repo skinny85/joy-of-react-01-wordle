4dbe7315aeafed814dea24d3e88ba4b55d9ba499
Adam Ruka <adamruka85@gmail.com>
2026-02-01 12:59:35 -0800

    Exercise 4 (guess coloring)

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -15,7 +15,7 @@ function Game() {
 
   return (
     <>
-      <PastGuesses guesses={guesses} />
+      <PastGuesses guesses={guesses} answer={answer} />
       <GuessInput addGuess={guess => {
         setGuesses([...guesses, guess]);
       }} />
--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,13 +1,15 @@
 import { range } from '../../utils';
+import { checkGuess } from '../../game-helpers';
 
 const NON_BREAKING_SPACE = '\xA0';
 
-function Guess({ guess = '' }) {
+function Guess({ guess, answer }) {
+  const guessStatus = checkGuess(guess, answer);
   return (
     <p className="guess">
       {range(5).map(index => (
-        <span className="cell" key={index}>
-          {guess[index] ?? NON_BREAKING_SPACE}
+        <span className={`cell ${guessStatus?.[index]?.status ?? ''}`} key={index}>
+          {guess?.[index] ?? NON_BREAKING_SPACE}
         </span>
       ))}
     </p>
