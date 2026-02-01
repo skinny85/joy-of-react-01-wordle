7b7d16c1b4f14367bec469ab5b23b0086a42ecd0
Adam Ruka <adamruka85@gmail.com>
2026-02-01 12:29:10 -0800

    Small corrections to exercise 3 after watching the solution video

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,10 +1,14 @@
 import { range } from '../../utils';
 
+const NON_BREAKING_SPACE = '\xA0';
+
 function Guess({ guess = '' }) {
   return (
     <p className="guess">
-      {range(0, 5).map(index => (
-        <span className="cell" key={index}>{guess[index]}</span>
+      {range(5).map(index => (
+        <span className="cell" key={index}>
+          {guess[index] ?? NON_BREAKING_SPACE}
+        </span>
       ))}
     </p>
   );
--- src/components/PastGuesses/PastGuesses.js
+++ src/components/PastGuesses/PastGuesses.js
@@ -5,7 +5,7 @@ import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 function PastGuesses({ guesses }) {
   return (
     <div className="guess-results">
-      {range(0, NUM_OF_GUESSES_ALLOWED).map(index => (
+      {range(NUM_OF_GUESSES_ALLOWED).map(index => (
         <Guess guess={guesses[index]} key={index} />
       ))}
     </div>
