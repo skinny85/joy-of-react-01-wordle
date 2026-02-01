4c88e86e9b06f9fd90da4e840280f9a1517755d0
Adam Ruka <adamruka85@gmail.com>
2026-02-01 00:35:02 -0800

    Small corrections to exercise 1 after solution video

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -6,7 +6,7 @@ function GuessInput() {
   return (
     <form className="guess-input-wrapper" onSubmit={event => {
       event.preventDefault();
-      console.log(guess);
+      console.log({ guess });
       setGuess('');
     }}>
       <label htmlFor="guess-input">Enter guess:</label>
@@ -17,6 +17,7 @@ function GuessInput() {
         onChange={event => {
           setGuess(event.target.value.toUpperCase());
         }}
+        title="Guess must be a 5-letter word"
       />
     </form>
   );
